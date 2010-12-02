5bd1e5bfc139668a8c854ef1e66401b3a496de35
hqhe <hqhe1982@126.com>
2010-12-02 08:01:42 -0500

    *) upgraded to nginx-0.9.1.

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -8,8 +8,8 @@
 #define _NGINX_H_INCLUDED_
 
 
-#define nginx_version         8053
-#define NGINX_VERSION      "0.8.53"
+#define nginx_version         9001
+#define NGINX_VERSION      "0.9.1"
 #define NGINX_VER          "nginx/" NGINX_VERSION
 
 #define NGINX_VAR          "NGINX"
